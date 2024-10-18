b306d1458ec1427927ead02fda4d383d0711abaf
sergiopuertas <sergiopuertasp@gmail.com>
2024-10-18 18:12:25 +0200

    data cleaning fixed

--- src/data_preparation/data_cleaning.py
+++ src/data_preparation/data_cleaning.py
@@ -1,20 +1,141 @@
 import nltk
 from nltk.corpus import wordnet
-from nltk.corpus import stopwords
 from ekphrasis.classes.preprocessor import TextPreProcessor
 from ekphrasis.dicts.emoticons import emoticons
 import pandas as pd
-import contractions
 import re
 import string
 
 # Inicialización de NLTK
 lmtzr = nltk.WordNetLemmatizer().lemmatize
 nltk.download("wordnet")
-nltk.download("stopwords")
 nltk.download("punkt")
-stop_words = set(stopwords.words("english"))
-
+stopwords = [
+    "i",
+    "me",
+    "my",
+    "myself",
+    "we",
+    "our",
+    "ours",
+    "ourselves",
+    "you",
+    "your",
+    "yours",
+    "yourself",
+    "yourselves",
+    "he",
+    "him",
+    "his",
+    "himself",
+    "she",
+    "her",
+    "hers",
+    "herself",
+    "it",
+    "its",
+    "itself",
+    "they",
+    "them",
+    "their",
+    "theirs",
+    "themselves",
+    "what",
+    "which",
+    "who",
+    "whom",
+    "this",
+    "that",
+    "these",
+    "those",
+    "am",
+    "is",
+    "are",
+    "was",
+    "were",
+    "be",
+    "been",
+    "being",
+    "have",
+    "has",
+    "had",
+    "having",
+    "do",
+    "does",
+    "did",
+    "doing",
+    "a",
+    "an",
+    "the",
+    "and",
+    "but",
+    "if",
+    "or",
+    "because",
+    "as",
+    "until",
+    "while",
+    "of",
+    "at",
+    "by",
+    "for",
+    "with",
+    "about",
+    "between",
+    "into",
+    "through",
+    "during",
+    "before",
+    "after",
+    "above",
+    "below",
+    "to",
+    "from",
+    "up",
+    "down",
+    "in",
+    "out",
+    "on",
+    "off",
+    "over",
+    "under",
+    "again",
+    "further",
+    "then",
+    "once",
+    "here",
+    "there",
+    "when",
+    "where",
+    "why",
+    "how",
+    "all",
+    "any",
+    "both",
+    "each",
+    "few",
+    "more",
+    "most",
+    "other",
+    "some",
+    "such",
+    "nor",
+    "only",
+    "own",
+    "same",
+    "so",
+    "than",
+    "too",
+    "very",
+    "s",
+    "t",
+    "can",
+    "will",
+    "just",
+    "don",
+    "should",
+    "now",
+]
 
 # Configuración de ekphrasis
 text_processor = TextPreProcessor(
@@ -32,10 +153,16 @@ text_processor = TextPreProcessor(
     corrector="twitter",
     unpack_hashtags=True,
     unpack_contractions=True,
-    spell_correct_elong=True,
+    spell_correct_elong=False,
     remove_tags=True,
     dicts=[emoticons],
 )
+
+
+def remove_stopwords(text, stopwords):
+    words = text.split()
+    filtered_words = [word for word in words if word.lower() not in stopwords]
+    return " ".join(filtered_words)
 
 
 # Función para cargar abreviaciones
@@ -47,12 +174,6 @@ def load_abbreviations(file_path):
                 abbr, expansion = line.split(":", 1)
                 abbreviation_dict[abbr.strip()] = expansion.strip()
     return abbreviation_dict
-
-
-# Función para eliminar stopwords
-def remove_stopwords(text):
-    filtered_words = [word for word in text if word not in stop_words]
-    return " ".join(filtered_words)
 
 
 # Obtener el tipo de palabra para lematización
@@ -80,7 +201,7 @@ def expand_abbreviations(text, abbreviation_dict):
 def normalize_text(text):
     word_pos = nltk.pos_tag(nltk.word_tokenize(text))
     lemm_words = [lmtzr(sw[0], get_wordnet_pos(sw[1])) for sw in word_pos]
-    return [x.lower() for x in lemm_words]
+    return " ".join([x.lower() for x in lemm_words])
 
 
 # Función principal de limpieza de texto
@@ -89,6 +210,7 @@ def clean_txt(text, abbreviation_dict):
     clean_text = "".join(
         text_processor.pre_process_doc(clean_text)
     )  # Procesar con ekphrasis
+    clean_text = expand_abbreviations(clean_text, abbreviation_dict)
     pattern = re.compile(
         r"[{}()\[\]<>*]"
     )  # Eliminar puntuación y caracteres no deseados
@@ -97,11 +219,8 @@ def clean_txt(text, abbreviation_dict):
     clean_text = "".join(clean_text)
 
     clean_text = " ".join(clean_text.split())  # Eliminar espacios extras
-    # Expandir abreviaciones
-    clean_text = expand_abbreviations(clean_text, abbreviation_dict)
-
+    clean_text = remove_stopwords(clean_text, stopwords)
     clean_text = normalize_text(clean_text)  # Tokenizar y lematizar
-    clean_text = remove_stopwords(clean_text)  # Eliminar stopwords
     return clean_text
 
 
